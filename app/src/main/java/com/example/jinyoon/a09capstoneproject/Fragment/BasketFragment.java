7d5e6427eb38b63ce2f6ec5fe42df0fe8f547d77
Jin Yoon <jinyoon0124@gmail.com>
2016-10-16 23:13:18 -0700

    Added recyclerView for basket page

--- app/src/main/java/com/example/jinyoon/a09capstoneproject/Fragment/BasketFragment.java
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/Fragment/BasketFragment.java
@@ -4,18 +4,27 @@ package com.example.jinyoon.a09capstoneproject.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.TextViewCompat;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckedTextView;
 import android.widget.TextView;
 
+import com.example.jinyoon.a09capstoneproject.BasketRecyclerViewAdapter;
+import com.example.jinyoon.a09capstoneproject.FridgeRecyclerViewAdapter;
 import com.example.jinyoon.a09capstoneproject.R;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BasketFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
 
     public BasketFragment() {
         // Required empty public constructor
@@ -27,7 +36,9 @@ public class BasketFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_basket, container, false);
-        TextView textView = (TextView) view.findViewById(R.id.basket_test_text);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_basket);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(new BasketRecyclerViewAdapter(getContext()));
 
         return view;
     }
