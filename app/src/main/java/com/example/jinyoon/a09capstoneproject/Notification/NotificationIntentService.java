baaaeed530f35eb5e256ffb356e69094066425e6
Jin Yoon <jinyoon0124@gmail.com>
2016-12-04 17:44:39 -0800

    Added remaining days calcluation logic

--- app/src/main/java/com/example/jinyoon/a09capstoneproject/Notification/NotificationIntentService.java
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/Notification/NotificationIntentService.java
@@ -3,13 +3,24 @@ package com.example.jinyoon.a09capstoneproject.Notification;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.util.TimeUtils;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.jinyoon.a09capstoneproject.Database.MyFridgeDataContract.*;
+import com.example.jinyoon.a09capstoneproject.Database.MyFridgeDataHelper;
 import com.example.jinyoon.a09capstoneproject.MainActivity;
 import com.example.jinyoon.a09capstoneproject.R;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
  *Notification build when intent is received
@@ -33,20 +44,74 @@ public class NotificationIntentService extends IntentService{
     protected void onHandleIntent(Intent intent) {
         String action =intent.getAction();
         if(ACTION_NOTIFICATION_SERVICE.equals(action)){
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-            builder.setContentTitle("TEST NOTIFICATION")
-                    .setAutoCancel(true)
-                    .setContentText("TEST NOTIFICATIOB CONTEXT TEXT")
-                    .setSmallIcon(R.drawable.ic_fridge_black);
+            //update all expiration based on current time... make sure to round up  numbers
+            long currentTimeinMil = System.currentTimeMillis();
 
-            PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                    NOTIFICATION_ID,
-                    new Intent(this, MainActivity.class),
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(pendingIntent);
 
-            NotificationManager manager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(NOTIFICATION_ID, builder.build());
+            Context context = getApplicationContext();
+
+            SQLiteDatabase db = new MyFridgeDataHelper(context).getReadableDatabase();
+            Cursor cursor =db.rawQuery("SELECT * FROM "+ FridgeListEntry.TABLE_NAME, null);
+
+            if(cursor.moveToFirst()){
+                while(!cursor.isAfterLast()){
+                    int id = cursor.getInt(cursor.getColumnIndex(FridgeListEntry._ID));
+
+                    long inputTimeinMil = Long.parseLong(cursor.getString(cursor.getColumnIndex(FridgeListEntry.COLUMN_INPUTDATEINMIL)));
+
+                    Log.e("!!!!!!!!!inputTime ", String.valueOf(inputTimeinMil));
+
+                    int newDays = (int) Math.ceil((long)(cursor.getInt(cursor.getColumnIndex(FridgeListEntry.COLUMN_EXPIRATION)))-(TimeUnit.MILLISECONDS.toDays(currentTimeinMil - inputTimeinMil)));
+
+                    Log.e("!!!!!!!!!!!newDay", String.valueOf(newDays));
+
+                    ContentValues cv = new ContentValues();
+                    cv.put(FridgeListEntry.COLUMN_EXPIRATION, newDays);
+                    cv.put(FridgeListEntry.COLUMN_INPUTDATEINMIL, String.valueOf(currentTimeinMil));
+
+                    context.getContentResolver().update(
+                            FridgeListEntry.CONTENT_URI,
+                            cv,
+                            FridgeListEntry._ID + " = ?",
+                            new String[]{String.valueOf(id)}
+                    );
+                    context.getContentResolver().notifyChange(FridgeListEntry.CONTENT_URI, null);
+
+                    cursor.moveToNext();
+
+                }
+            }
+
+
+            cursor = context.getContentResolver().query(
+                    FridgeListEntry.CONTENT_URI,
+                    null,
+                    FridgeListEntry.COLUMN_EXPIRATION +" <= ? ",
+                    new String[]{"1"},
+                    null
+            );
+
+            if(cursor ==null || cursor.getCount()!=0){
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                builder.setContentTitle(context.getString(R.string.app_name))
+                        .setAutoCancel(true)
+                        .setContentText("ITEM IS ABOUT TO EXPIRE... CLICK TO CHECK RECIPE")
+                        .setSmallIcon(R.drawable.ic_fridge_black);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                        NOTIFICATION_ID,
+                        new Intent(this, MainActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+
+                NotificationManager manager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(NOTIFICATION_ID, builder.build());
+            }else{
+                Log.e(LOG_TAG, "No item is about to expire");
+            }
+
+
+            cursor.close();
         }
     }
 }
