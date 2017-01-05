4fe23fc3dad8d00967efa89bd1297446b2ff9cbf
Jin Yoon <jinyoon0124@gmail.com>
2017-01-05 00:05:10 -0800

    ANALYTICS - application subclass is created - name attribute is added with the name of the subclass

---
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.jinyoon.a09capstoneproject;
+
+import android.app.Application;
+
+/**
+ * Application subclass for Analytics
+ */
+
+public class MyApplication extends Application {
+}
