834ba315b9daab90c6cab88cb670e11ca679613e
Jin Yoon <jinyoon0124@gmail.com>
2017-01-05 00:12:40 -0800

    ANALYTICS - starttracking() and gettracker() methods implemented in application subclass

--- app/src/main/java/com/example/jinyoon/a09capstoneproject/MyApplication.java
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/MyApplication.java
@@ -2,9 +2,25 @@ package com.example.jinyoon.a09capstoneproject;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Application subclass for Analytics
  */
 
 public class MyApplication extends Application {
+    //Get the tracker associated with this app
+    public Tracker mTracker;
+    public void startTracking(){
+        if(mTracker==null){
+            GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+            mTracker=ga.newTracker(R.xml.track_app);
+            ga.enableAutoActivityReports(this);
+        }
+    }
+    public Tracker getTracker(){
+        startTracking();
+        return mTracker;
+    }
 }
