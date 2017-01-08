c18d7b4f2686cb430ee039ac65a1106bcad9bd7a
Jin Yoon <jinyoon0124@gmail.com>
2017-01-07 16:39:17 -0800

    ANALYTICS - add screen view tracker to all fragment and detail activity

--- app/src/main/java/com/example/jinyoon/a09capstoneproject/RecipeDetailActivity.java
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/RecipeDetailActivity.java
@@ -9,7 +9,12 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
 public class RecipeDetailActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl(getIntent().getExtras().getString(getString(R.string.publisher_url_key)));
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //Get tracker.
+        Tracker tracker = ((MyApplication) getApplication()).getTracker();
+
+        //Set screen name
+        tracker.setScreenName(LOG_TAG);
+        //Send a screen view.
+        tracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
 }
