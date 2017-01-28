b03ab4dd4c963b50bee184924348c0e76c0d6926
Jin Yoon <jinyoon0124@gmail.com>
2017-01-28 13:21:27 -0800

    fix: Change notification alarm manager
    Change AlarmManager.setRepeating to AlarmManager.set method as alarm triggers everyday at the set time even without the repeating parameter. AlarmManager.setExactAndAllowWhileIdle is used for SDK >= 23 because alarm was not triggered when the device is in Doze mode. For SDK versions lower than 23 but higher than 19, setExact method is used as Doze mode was not introduced in those versions.

--- app/src/main/java/com/example/jinyoon/a09capstoneproject/Notification/NotificationEventReceiver.java
+++ app/src/main/java/com/example/jinyoon/a09capstoneproject/Notification/NotificationEventReceiver.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
@@ -39,11 +40,25 @@ public class NotificationEventReceiver extends WakefulBroadcastReceiver{
         //set up alarmManager
         //In order to trigger everyday at 4:30 pm, use AlarmManager.INTERVAL_DAY for repeat
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
-                triggerAt,
-                AlarmManager.INTERVAL_DAY,
-                notificationPendingIntent
-        );
+
+        if(Build.VERSION.SDK_INT >= 23){
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
+                    triggerAt,
+                    notificationPendingIntent);
+        }else if(Build.VERSION.SDK_INT >=19){
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP,
+                    triggerAt,
+                    notificationPendingIntent);
+        }else{
+            alarmManager.set(AlarmManager.RTC_WAKEUP,
+                    triggerAt,
+                    notificationPendingIntent);
+        }
+//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+//                triggerAt,
+//                AlarmManager.INTERVAL_DAY,
+//                notificationPendingIntent
+//        );
 
     }
 
